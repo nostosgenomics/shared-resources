7d1683fa2d7f2a5928db7861d0d7d7a867e6551b
Max Schelker <max.schelker@nostos-genomics.com>
2025-05-19 16:43:14 +0200

    use black instead of yapf

--- shared_resources/dictionaries/dictionaries.py
+++ shared_resources/dictionaries/dictionaries.py
@@ -1,6 +1,7 @@
 """
 Utility to create resources dictionaries from csv or tsv files.
 """
+
 import logging
 from datetime import datetime as dt
 from functools import reduce
@@ -10,7 +11,7 @@ from shared_resources.dictionaries.utils import load_json
 
 logger = logging.getLogger(__name__)
 
-BASE_DIR = Path(__file__).parent.parent / 'data'
+BASE_DIR = Path(__file__).parent.parent / "data"
 HPO_JSON = BASE_DIR / "hpo_dictionary.json"
 ROSETTA_SYMBOLS_JSON = BASE_DIR / "rosetta_symbol_to_hgnc_id.json"
 ROSETTA_IDS_JSON = BASE_DIR / "rosetta_hgnc_id_to_symbol.json"
@@ -30,18 +31,17 @@ class ResourcesDictionaries:
     @staticmethod
     def get_fullname(hpo):
         fullname = hpo["name_id"]
-        synonyms = [[w for w in s.split(" ") if w.lower() not in [x.lower() for x in fullname.split(" ")]] for s in hpo["synonyms"]
-                    if s and not s.startswith("HP:")]
+        synonyms = [
+            [w for w in s.split(" ") if w.lower() not in [x.lower() for x in fullname.split(" ")]]
+            for s in hpo["synonyms"]
+            if s and not s.startswith("HP:")
+        ]
         return (f"{fullname} " + (" ".join(sorted(set(reduce(list.__add__, synonyms)))) if synonyms else "")).lower()
 
     def _load(self):
         hpo_dict = load_json(HPO_JSON)
         self.hpo_dict = {
-            k: {
-                "name": v["name_id"],
-                "full_name": self.get_fullname(v),
-                "children": v.get("children", [])
-            }
+            k: {"name": v["name_id"], "full_name": self.get_fullname(v), "children": v.get("children", [])}
             for k, v in hpo_dict.items()
         }
         self.symbol_to_hgnc = load_json(ROSETTA_SYMBOLS_JSON)
@@ -55,23 +55,29 @@ class ResourcesDictionaries:
     @property
     def hpo_list(self):
         self._reload()
-        hpo_dict_list = [{
-            "id": k,
-            "name": v["name"],
-            "full_name": v["full_name"],
-            "synonyms": v.get("synonyms", []),
-            "children": v.get("children", []),
-        } for k, v in self.hpo_dict.items()]
+        hpo_dict_list = [
+            {
+                "id": k,
+                "name": v["name"],
+                "full_name": v["full_name"],
+                "synonyms": v.get("synonyms", []),
+                "children": v.get("children", []),
+            }
+            for k, v in self.hpo_dict.items()
+        ]
         return sorted(hpo_dict_list, key=lambda h: h.get("id"))
 
     def get_hpo_items(self, hpo_list):
-        return [{
-            "id": hpo_id,
-            "name": self.hpo_dict.get(hpo_id, {}).get("name"),
-            "full_name": self.hpo_dict.get(hpo_id, {}).get("full_name"),
-            "synonyms": self.hpo_dict.get(hpo_id, {}).get("synonyms", []),
-            "children": self.hpo_dict.get(hpo_id, {}).get("children", []),
-        } for hpo_id in sorted(set(hpo_list))]
+        return [
+            {
+                "id": hpo_id,
+                "name": self.hpo_dict.get(hpo_id, {}).get("name"),
+                "full_name": self.hpo_dict.get(hpo_id, {}).get("full_name"),
+                "synonyms": self.hpo_dict.get(hpo_id, {}).get("synonyms", []),
+                "children": self.hpo_dict.get(hpo_id, {}).get("children", []),
+            }
+            for hpo_id in sorted(set(hpo_list))
+        ]
 
 
 resources_dictionaries = ResourcesDictionaries()
